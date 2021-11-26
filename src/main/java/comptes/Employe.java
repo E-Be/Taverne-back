0139c233e3cfa692e9cbe711df421daa671f0464
Antoine SPANIER <antoine.spanier@gmail.com>
2021-11-26 16:49:07 +0100

    Modifs Cardinalités
    sur boisson, bar, utilisation, employé, events, horaires

--- src/main/java/comptes/Employe.java
+++ src/main/java/comptes/Employe.java
@@ -1,10 +1,22 @@
 package comptes;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import fonctionnalitees.Events;
+import inventaire.Bar;
 
 @Entity
 public class Employe extends Compte {
-
+	
+	@ManyToOne
+	private Bar bar;
+	
+	@OneToMany(mappedBy = "emp")
+	private List<Events> events;
 	
 	public Employe() {}
 	
