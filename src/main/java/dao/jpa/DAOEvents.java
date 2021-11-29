de149bbaafe2a73bce5195a3b8f6147da6ca4b01
Antoine SPANIER <antoine.spanier@gmail.com>
2021-11-29 12:17:16 +0100

    Auto stash before merge of "Develop" and "origin/Develop"

--- src/main/java/dao/jpa/DAOEvents.java
+++ src/main/java/dao/jpa/DAOEvents.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
 import fonctionnalitees.Events;
+import idao.jpa.IDAOEvents;
 import inventaire.Article;
 import idao.jpa.IDAOEvents;
 import util.Context;
