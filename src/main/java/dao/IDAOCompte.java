c8a149ce29a523922fd090d46a95943147156fd1
Erwan <erwan.beving@gmail.com>
2021-11-29 12:03:22 +0100

    Updated IDAOCompte

--- src/main/java/dao/IDAOCompte.java
+++ src/main/java/dao/IDAOCompte.java
@@ -1,9 +1,13 @@
 package dao;
 
+import java.util.List;
+
 import comptes.Compte;
 
 public interface IDAOCompte extends IDAO<Compte, Integer> {
 
 	
 		public Compte connect (String login,String password);
+		
+		public List<Compte> findByTypeCompte (String typeCompte);
 }
