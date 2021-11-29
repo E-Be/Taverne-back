62e8a5295b97d1e95667d544bc93d4c2aaea65bd
Antoine SPANIER <antoine.spanier@gmail.com>
2021-11-29 14:30:03 +0100

    Ajout findByMinPts dans CarteFidelite

--- src/main/java/idao/jpa/IDAOCarteFidelite.java
+++ src/main/java/idao/jpa/IDAOCarteFidelite.java
@@ -1,7 +1,11 @@
 package idao.jpa;
 
+import java.util.List;
+
 import fonctionnalitees.CarteFidelite;
 
 public interface IDAOCarteFidelite extends IDAO<CarteFidelite,Integer>{
+	
+	public List<CarteFidelite> findAllByMinPts(int minPts);
 
 }
